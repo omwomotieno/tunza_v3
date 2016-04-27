43275cf77b90a048b19ff1d7955ddb23b4c6acc6
omwomotieno <wilmwomo@gmail.com>
2016-04-27 13:48:47 +0300

    changes made. Call_back and current_time not dynamic

--- call_engine/urls.py
+++ call_engine/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from call_engine import views
 
 urlpatterns = [
-	url(r'^(?P<id>\d+)/call_back/$', views.voice_callback, name='call_back'),
-    url(r'^(?P<id>\d+)/current_time/$', views.check_time, name='current_time'),
+	url(r'^call_back/$', views.voice_callback, name='call_back'),
+    url(r'^current_time/$', views.check_time, name='current_time'),
     url(r'^service_url/$', views.from_number_to_url, name='service_url'),
 ]
