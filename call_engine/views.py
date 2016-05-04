8be44646785e0b0ef8b90b01f0973a53b8ca1260
omwomotieno <wilmwomo@gmail.com>
2016-05-04 11:38:16 +0300

    Optimized call_engine.views .. Check time running inside a cronbase .. To be run through manage.py runcrons

--- call_engine/views.py
+++ call_engine/views.py
@@ -25,8 +25,8 @@ def voice_callback(request):
         # queryset_call = Call_Response.objects.all()
 
         # retrieving call_back params
-        is_active = str(0)  # request.GET.get('isActive', '')
-        callTo = '+254700050144'  # request.GET.get('callerNumber', '')
+        is_active = str(1)  # request.GET.get('isActive', '')
+        callTo = '+254720955704'  # request.GET.get('callerNumber', '')
         callTo = str(callTo)
 
         # retrieving service url from callerNumber
@@ -74,36 +74,6 @@ def voice_callback(request):
             return resp
 
 
-def check_time(request):
-    username = "OtisKe"
-    apikey = "07984423a278ead54fee35d3daf956598deb51405b27fe70f1e2dfe964be5c04"
-    callFrom = "+254711082079"
-    message = "We tried calling you to inform you of the appointment. We could not reach you."
-    gateway = AfricasTalkingGateway(username, apikey)
-
-    today = datetime.now().date()
-    difference = timedelta(days=1)
-    call_date = today + difference
-
-    current_time = datetime.now().time()
-    time_format = '%H:%M'
-    curr_time = current_time.strftime(time_format)
-
-    query_set = get_list_or_404(
-        Reminder,
-        appointment_date=call_date,
-        set_on=True,
-        time_of_call=curr_time,
-    )
-
-    for instance in query_set:
-        instance_contact = instance.patient.patient_contact
-        callTo = instance_contact
-        gateway.call(callFrom, callTo)
-
-    return HttpResponse("Calls successfully placed")
-
-
 class TunzaCronJob(CronJobBase):
     RUN_EVERY_MINS = 1
 
@@ -137,13 +107,3 @@ class TunzaCronJob(CronJobBase):
             gateway.call(callFrom, callTo)
 
         return HttpResponse("Calls successfully placed")
-
-
-def from_number_to_url(request):
-    callTo = '+254700050144'
-    call_number = callTo
-    # request.GET.get('callerNumber', '')
-    patient_contact = Reminder.objects.filter(patient__patient_contact=call_number)
-    instance = patient_contact.values_list('service__service_url')
-    print instance[0][0]
-    return HttpResponse('found')
